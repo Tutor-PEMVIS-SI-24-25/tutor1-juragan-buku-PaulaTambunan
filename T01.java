a0dc8ad43473fc1c8e2d41627eea9d65095ef898
PaulaTambunan <paulatambunan10@gmail.com>
2024-09-04 21:27:02 +0700

    Telah Menyelesaikan Tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,15 @@
-// NIM - NAMA
-// NIM - NAMA
+12S24025 - Paula Gevriella Tambunan
+12S24025 - Paula Gevriella Tambunan
+import java.util.*;
+import java.lang.Math;
 
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
